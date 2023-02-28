08d4f6a7e1f4cba5c924acb621897f794d3277a7
Ankit Dhiman <63338248+ankitdhiman47704@users.noreply.github.com>
2023-02-28 21:43:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your code here
+let pageNumber = 1;
+let dataDiv = document.getElementById('data')
+let showPage = document.getElementById("showPageNumber")
+async function fetchData(pageNum){
+    let response = await fetch(`https://api.github.com/repositories/1296269/issues?page=${pageNum}&per_page=5`)
+    if(response){
+        let data = await response.json()
+        data.map((value)=>{
+            dataDiv.innerHTML +=`<li>${value.title}</li>` 
+        })
+    }
+} 
+function nextPage(){
+    dataDiv.innerHTML = ""
+    document.getElementById("load_prev").disabled = false
+    pageNumber++;
+    showPage.innerText = pageNumber
+    fetchData(pageNumber);
+}
+function prevPage(){
+    dataDiv.innerHTML = ""
+    if(pageNumber==2){
+        document.getElementById("load_prev").disabled = true
+    }else{
+        document.getElementById("load_prev").disabled = false
+    }
+        pageNumber--;
+        showPage.innerText = pageNumber
+        fetchData(pageNumber);
+      
+}
+fetchData(1)
